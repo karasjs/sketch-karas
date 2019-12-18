de50f7ed2584a76cae5c6bbe840d0be214a1f715
army8735 <army8735@qq.com>
2019-12-18 22:07:51 +0800

    feat: clearLibrary

---
+++ src/clearLibrary.js
@@ -0,0 +1,18 @@
+import { Document } from 'sketch/dom';
+import UI from 'sketch/ui';
+import Settings from 'sketch/settings';
+
+export function clearLibrary() {
+  let document = Document.getSelectedDocument();
+  UI.getInputFromUser('Confirm to clear Library(Y/N)', {
+    initialValue: 'N',
+  }, (err, value) => {
+    if(err) {
+      return;
+    }
+    if(value === 'Y') {
+      Settings.setDocumentSettingForKey(document, 'library', null);
+      UI.message('Library cleared');
+    }
+  });
+}
